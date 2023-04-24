537cd5b9eb5b2c0e2b946fb976dbe90c4eecb3b1
HYO00 <hyo02222@gmail.com>
2023-04-24 13:53:53 +0900

    reactQuery, Recoil, GraphQL 추가

--- pangyogo/src/pages/_app.tsx
+++ pangyogo/src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { QueryClient, QueryClientProvider } from "react-query";
+import { RecoilRoot } from 'recoil';
+
+// Create a client
+const queryClient = new QueryClient()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </RecoilRoot>
+  ) 
 }
