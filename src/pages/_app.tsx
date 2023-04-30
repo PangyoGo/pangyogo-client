3e06d7fac149322bbb993a79ffbab391088add84
hyo0 <hyo0@hyo0ui-MacBookPro.local>
2023-04-30 17:17:24 +0900

    login test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,29 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { QueryClient, QueryClientProvider } from "react-query";
+import { QueryClient, QueryClientProvider} from "react-query";
+import { ReactQueryDevtools } from 'react-query/devtools'
 import { RecoilRoot } from 'recoil';
 
 // Create a client
 const queryClient = new QueryClient()
 
 if (process.env.NODE_ENV === 'development') {
-  import('../mocks/browser')
-    .then(({ worker }) => {
+  if (typeof window === 'undefined') {
+    import('../mocks/server').then(({ worker }) => {
+      worker.listen();
+    });
+  } else {
+    import('../mocks/browser').then(({ worker }) => {
       worker.start();
     });
+  }
 }
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
       <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools />
         <Component {...pageProps} />
       </QueryClientProvider>
     </RecoilRoot>
