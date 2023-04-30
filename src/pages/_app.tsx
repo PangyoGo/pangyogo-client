566085f7a6c8c685b3a555755e80850ee90764a2
= <=>
2023-04-30 17:29:05 +0900

    login test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,29 +1,22 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { QueryClient, QueryClientProvider} from "react-query";
-import { ReactQueryDevtools } from 'react-query/devtools'
+import { QueryClient, QueryClientProvider } from "react-query";
 import { RecoilRoot } from 'recoil';
 
 // Create a client
 const queryClient = new QueryClient()
 
 if (process.env.NODE_ENV === 'development') {
-  if (typeof window === 'undefined') {
-    import('../mocks/server').then(({ worker }) => {
-      worker.listen();
-    });
-  } else {
-    import('../mocks/browser').then(({ worker }) => {
+  import('../mocks/browser')
+    .then(({ worker }) => {
       worker.start();
     });
-  }
 }
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
       <QueryClientProvider client={queryClient}>
-        <ReactQueryDevtools />
         <Component {...pageProps} />
       </QueryClientProvider>
     </RecoilRoot>
--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -1,30 +1,23 @@
 import { ApolloServer } from '@apollo/server';
 import { startServerAndCreateNextHandler } from '@as-integrations/next';
 import { gql } from 'graphql-tag';
-import { UserInfo } from '../login';
 
 //스키마 타입 정의 
 const typeDefs = gql`
 	type User {
 		id: ID
-		nickname: String
+		firstName: String
+		lastName: String
   }
 	type Query {
 		getUser: User
-  }
-
-  type Mutation {
-    user(id:String, password:String): User
   }
 `;
 //GraphQL 스키마를 통해 제공할 데이터를 정의하는 함수를 담은 객체를 할당
 const resolvers = {
     Query: {
-      getUser: () => ({ id: "1", nickname: "World"}),
+      getUser: () => ({ id: "1", firstName: "Hello", lastName: "World" }),
     },
-    Mutation: {
-      user: ():UserInfo => ({ id: "hyo0", nickname:"하하호호"})
-    }
   };
 
 const apolloServer = new ApolloServer({
