acf3497ff20c5debb500cafd0f9d81572c5bec5c
suld2495 <suld7777@gmail.com>
2023-05-23 20:21:38 +0900

    사용되지 않는 코드 삭제

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,40 +1,9 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import type { NextPage } from "next";
-import { useQuery } from "react-query";
-import { GraphQLClient, gql } from "graphql-request";
-
-const inter = Inter({ subsets: ['latin'] })
-
-const UserQuery = gql`
-  query getUser {
-    getUser {
-     id
-     firstName
-     lastName
-    }
-  }
-`;
-
-const graphQLClient = new GraphQLClient("http://localhost:3000/api/graphql");
-const fetchUser = async () => {
-  return await graphQLClient.request(UserQuery);
-};
 
 const Home: NextPage = () => {
-  const { isLoading, data } = useQuery(["get-user"], fetchUser);
-
-  if (isLoading) return <p>Loading...</p>;
-
   return (
     <>
-      <div className="flex h-screen w-screen justify-center items-center">
-        <div>
-          <p>{data.getUser.id}</p>
-          <p>{data.getUser.firstName}</p>
-					<p>{data.getUser.lastName}</p>
-        </div>
-      </div>
+      HELLO WORLD
     </>
   );
 };
